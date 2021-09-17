346e0cb5cd232d0250d5ce5c5768ed3cf8a5667a
Gabe <thetiemann01@gmail.com>
2021-09-16 23:39:02 -0500

    rotation mods

--- Assets/Rotation.cs
+++ Assets/Rotation.cs
@@ -14,20 +14,54 @@ public class Rotation : MonoBehaviour
 {
     float smooth = 5.0f;
     float tiltAngle = 60.0f;
+    private float movementSpeed = 5f;
 
     public GameObject rotationTextObject;
-    public string[] PhraseList = new string[3] { "good", "bad", "meh" };
+    public string[] PhraseList = new string[4] { "good", "bad", "meh", "other words" };
 
     System.Random rand = new System.Random();
+
+    float rotationSpeed = 45;
+    Vector3 currentEulerAngles;
+    float x;
+    float y;
+    float z;
+    Vector3 oldEulerAngles;
+    public Transform target;
+    int index;
+    void Start(){
+        oldEulerAngles = transform.rotation.eulerAngles;
+    }
+
     void Update()
     { 
-        if (transform.hasChanged)
-        {
-            print("The transform has changed!");
-            transform.hasChanged = false;
-            int index = rand.Next(PhraseList.Length);
+        if ((transform.rotation.eulerAngles.x <= (oldEulerAngles.x + 0.1f) && transform.rotation.eulerAngles.x >= (oldEulerAngles.x - 0.1f)) ||
+            (transform.rotation.eulerAngles.y <= (oldEulerAngles.x + 0.1f) && transform.rotation.eulerAngles.y >= (oldEulerAngles.x - 0.1f)) ||
+            (transform.rotation.eulerAngles.z <= (oldEulerAngles.x + 0.1f) && transform.rotation.eulerAngles.z >= (oldEulerAngles.x - 0.1f)) ){
+        
+        // Vector3 targetDir = target.position - transform.position;
+        // float angle = Vector3.Angle(targetDir, transform.forward);
+        // if (angle < 5.0f){
+           //NO ROTATION
+            index = rand.Next(PhraseList.Length);
             rotationTextObject.GetComponent<TextMeshPro>().text = PhraseList[index];
+            // rotationTextObject.GetComponent<TextMeshPro>().text = "No change";
+        } else{
+            oldEulerAngles = transform.rotation.eulerAngles;
+            // rotationTextObject.GetComponent<TextMeshPro>().text = transform.rotation.ToString();
+            int index2 = rand.Next(PhraseList.Length);
+            if (index2 != index){
+                rotationTextObject.GetComponent<TextMeshPro>().text = PhraseList[index];
+            }
+            //DO WHATEVER YOU WANT
         }
+        // if (transform.hasChanged)
+        // {
+        //     print("The transform has changed!");
+        //     transform.hasChanged = false;
+        //     int index = rand.Next(PhraseList.Length);
+        //     rotationTextObject.GetComponent<TextMeshPro>().text = PhraseList[index];
+        // }
 
         // Smoothly tilts a transform towards a target rotation.
         // float tiltAroundZ = Input.GetAxis("Horizontal") * tiltAngle;
@@ -39,9 +73,23 @@ public class Rotation : MonoBehaviour
         // // Dampen towards the target rotation
         // transform.rotation = Quaternion.Slerp(transform.rotation, target,  Time.deltaTime * smooth);
 
-        // if (){
-        //     int index = rand.Next(PhraseList.Length);
-        //     rotationTextObject.GetComponent<TextMeshPro>().text = PhraseList[index];
-        // }
+        // rotationTextObject.GetComponent<TextMeshPro>().text = transform.rotation.ToString();
+        // // if (){
+        //     // int index = rand.Next(PhraseList.Length);
+        //     // rotationTextObject.GetComponent<TextMeshPro>().text = PhraseList[index];
+        // // }
+
+        // //get the Input from Horizontal axis
+        // float horizontalInput = Input.GetAxis("Horizontal");
+        // //get the Input from Vertical axis
+        // float verticalInput = Input.GetAxis("Vertical");
+
+        // //update the position
+        // transform.position = transform.position + new Vector3(horizontalInput * movementSpeed * Time.deltaTime, verticalInput * movementSpeed * Time.deltaTime, 0);
+
+        // // rotationTextObject.GetComponent<TextMeshPro>().text = transform.position.ToString();
+        // //output to log the position change
+        // Debug.Log(transform.position);
     }
 }
+
