805ffb6a3022ae44e0e80104e1601f449d0e43dc
Gabe <thetiemann01@gmail.com>
2021-09-18 18:11:14 -0500

    Some changes to magic 8, temp not displaying on mobile

--- Assets/Scripts/Rotation.cs
+++ Assets/Scripts/Rotation.cs
@@ -17,7 +17,33 @@ public class Rotation : MonoBehaviour
     private float movementSpeed = 5f;
 
     public GameObject rotationTextObject;
-    public string[] PhraseList = new string[4] { "good", "bad", "meh", "other words" };
+    public string[] PhraseList = new string[20] { 
+        // Good outcomes
+        "It is Certain",
+        "It is decidedly so",
+        "Without a doubt",
+        "Yes definitely",
+        "You may rely on it",
+        "As I see it, yes",
+        "Most likely",
+        "Outlook good",
+        "Yes",
+        "Signs point to yes",
+
+        // Unclear outcomes
+        "Reply hazy, try again",
+        "Ask again later",
+        "Better not tell you now",
+        "Cannot predict now",
+        "Concentrate and ask again",
+
+        // Negative outcomes
+        "Don't count on it",
+        "My reply is no",
+        "My sources say no",
+        "Outlook not so good",
+        "Very doubtful"
+    };
 
     System.Random rand = new System.Random();
 
@@ -35,24 +61,29 @@ public class Rotation : MonoBehaviour
 
     void Update()
     { 
-        if ((transform.rotation.eulerAngles.x <= (oldEulerAngles.x + 0.1f) && transform.rotation.eulerAngles.x >= (oldEulerAngles.x - 0.1f)) ||
-            (transform.rotation.eulerAngles.y <= (oldEulerAngles.x + 0.1f) && transform.rotation.eulerAngles.y >= (oldEulerAngles.x - 0.1f)) ||
-            (transform.rotation.eulerAngles.z <= (oldEulerAngles.x + 0.1f) && transform.rotation.eulerAngles.z >= (oldEulerAngles.x - 0.1f)) ){
+        // if ((transform.rotation.eulerAngles.x <= (oldEulerAngles.x + 0.1f) && transform.rotation.eulerAngles.x >= (oldEulerAngles.x - 0.1f)) ||
+        //     (transform.rotation.eulerAngles.y <= (oldEulerAngles.x + 0.1f) && transform.rotation.eulerAngles.y >= (oldEulerAngles.x - 0.1f)) ||
+        //     (transform.rotation.eulerAngles.z <= (oldEulerAngles.x + 0.1f) && transform.rotation.eulerAngles.z >= (oldEulerAngles.x - 0.1f)) ){
+            if ((Math.Abs(transform.rotation.eulerAngles.x - oldEulerAngles.x) > 90f ) ||
+                (Math.Abs(transform.rotation.eulerAngles.y - oldEulerAngles.y) > 90f ) ||
+                (Math.Abs(transform.rotation.eulerAngles.z - oldEulerAngles.z) > 90f ) ){
+        // if (Math.Abs(oldEulerAngles - transform.rotation.eulerAngles) > 90){
         
         // Vector3 targetDir = target.position - transform.position;
         // float angle = Vector3.Angle(targetDir, transform.forward);
         // if (angle < 5.0f){
-           //NO ROTATION
-            index = rand.Next(PhraseList.Length);
-            rotationTextObject.GetComponent<TextMeshPro>().text = PhraseList[index];
-            // rotationTextObject.GetComponent<TextMeshPro>().text = "No change";
-        } else{
+
             oldEulerAngles = transform.rotation.eulerAngles;
             // rotationTextObject.GetComponent<TextMeshPro>().text = transform.rotation.ToString();
             int index2 = rand.Next(PhraseList.Length);
             if (index2 != index){
                 rotationTextObject.GetComponent<TextMeshPro>().text = PhraseList[index];
             }
+        } else{
+           //NO ROTATION
+            // index = rand.Next(PhraseList.Length);
+            // rotationTextObject.GetComponent<TextMeshPro>().text = PhraseList[index];
+            // rotationTextObject.GetComponent<TextMeshPro>().text = "No change";
             //DO WHATEVER YOU WANT
         }
         // if (transform.hasChanged)
